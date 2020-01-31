e0e55ba3d80f6050309c14c26f221da68fbca15c
dyoshikawa <dyoshikawa1993@gmail.com>
2020-01-31 12:31:42 +0900

    add

--- watabe/src/lib.rs
+++ watabe/src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 
 #[allow(dead_code)]
 fn p269(n: i32, vss: &[&[i32]]) -> Vec<Vec<i32>> {
@@ -24,7 +24,7 @@ fn p269(n: i32, vss: &[&[i32]]) -> Vec<Vec<i32>> {
 }
 
 #[test]
-fn test_p269() {
+fn p269_test() {
     let n = 4;
     let vss: &[&[i32]] = &[&[1, 2, 2, 4], &[2, 1, 4], &[3, 0], &[4, 1, 3]];
     let expected = vec![
@@ -77,7 +77,7 @@ fn p273(n: i32, argss: &[&[i32]]) -> Vec<Vec<i32>> {
 }
 
 #[test]
-fn test_p273() {
+fn p273_test() {
     let n = 6;
     let argss: &[&[i32]] = &[
         &[1, 2, 2, 3],
@@ -96,4 +96,49 @@ fn test_p273() {
         vec![5, 6],
     ];
     assert_eq!(p273(n, argss), expected);
+}
+
+#[allow(dead_code)]
+fn p282(n: i32, vss: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    let mut graph: HashMap<i32, Vec<i32>> = HashMap::new();
+    for (i, vs) in (0_i32..).zip(vss.iter()) {
+        graph.insert(i, vec![]);
+        for (j, v) in (0_i32..).zip(vs.iter()) {
+            if [0, 1].contains(&j) {
+                continue;
+            }
+            graph.get_mut(&i).unwrap().push(v - 1);
+        }
+    }
+
+    let mut visited: Vec<bool> = vec![false; n as usize];
+    let mut distances: Vec<i32> = vec![-1; n as usize];
+    distances[0] = 0;
+    let mut dequeue: VecDeque<i32> = VecDeque::new();
+    dequeue.push_front(0);
+    while !dequeue.is_empty() {
+        let now = dequeue.pop_back().unwrap();
+        for i in graph.get(&now).unwrap().iter() {
+            if !visited[*i as usize] {
+                distances[*i as usize] = distances[now as usize] + 1;
+                dequeue.push_front(*i);
+                visited[*i as usize] = true;
+            }
+        }
+    }
+
+    let mut res: Vec<Vec<i32>> = vec![vec![0; 2]; n as usize];
+    for (i, distance) in (0_i32..).zip(distances.iter()) {
+        res[i as usize][0] = i + 1;
+        res[i as usize][1] = *distance;
+    }
+    res
+}
+
+#[test]
+fn p282_test() {
+    let n = 4;
+    let vss = vec![vec![1, 2, 2, 4], vec![2, 1, 4], vec![3, 0], vec![4, 1, 3]];
+    let expected = vec![vec![1, 0], vec![2, 1], vec![3, 2], vec![4, 1]];
+    assert_eq!(p282(n, vss), expected);
 }
