30143c9b89b3b5cddb5c2a7b6a29e8651872eae6
dyoshikawa <dyoshikawa1993@gmail.com>
2020-03-08 21:08:37 +0900

    add

--- src/watabe/p142.rs
+++ src/watabe/p142.rs
@@ -51,3 +51,46 @@ fn test() {
         ]
     );
 }
+
+#[allow(dead_code)]
+struct Main {
+    n: i32,
+    q: i32,
+    a: Vec<i32>,
+    m: Vec<i32>,
+}
+
+#[allow(dead_code)]
+impl Main {
+    fn solve(&self, i: i32, v: i32) -> bool {
+        if v == 0 {
+            return true;
+        }
+        if i >= self.n {
+            return false;
+        }
+        self.solve(i + 1, v) || self.solve(i + 1, v - self.a[i as usize])
+    }
+
+    fn main(&self) -> Vec<bool> {
+        let mut res = vec![false; self.q as usize];
+        for (i, v) in self.m.iter().enumerate() {
+            res[i] = self.solve(0, *v);
+        }
+        res
+    }
+}
+
+#[test]
+fn invoke_test() {
+    assert_eq!(
+        Main {
+            n: 5,
+            q: 4,
+            a: vec![1, 5, 7, 10, 21],
+            m: vec![2, 4, 17, 8]
+        }
+        .main(),
+        vec![false, false, true, true]
+    );
+}
