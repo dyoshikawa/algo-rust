49c7e74c7a9a3616a58faf1d6089c92c19e47fc1
dyoshikawa <dyoshikawa1993@gmail.com>
2020-03-30 18:49:55 +0900

    mod

--- src/watabe/p198.rs
+++ src/watabe/p198.rs
@@ -1,3 +1,5 @@
+// TODO 優先度低いので保留
+
 #[allow(dead_code)]
 struct Main {
     n: usize,
@@ -7,14 +9,12 @@ struct Main {
     post_res: Vec<Option<usize>>,
 }
 
-#[allow(dead_code)]
+#[allow(dead_code, unused_variables)]
 impl Main {
-    fn pre_parse(u: Option<usize>) {
+    fn pre_parse(&self, u: Option<usize>) {
         match u {
             None => (),
-            Some(u) => {
-
-            }
+            Some(u) => {}
         }
     }
 
@@ -25,27 +25,27 @@ impl Main {
     fn main() {}
 }
 
-#[test]
-fn main_test() {
-    let n = 9;
-    assert_eq!(
-        Main {
-            n: n,
-            a: vec![
-                vec![0, 1, 4],
-                vec![1, 2, 3],
-                vec![2, -1, -1],
-                vec![3, -1, -1],
-                vec![4, 5, 8],
-                vec![5, 6, 7],
-                vec![6, -1, -1],
-                vec![7, -1, -1],
-                vec![8, -1, -1],
-            ],
-            pre_res: vec![None; n],
-            in_res: vec![None; n],
-            post_res: vec![None; n],
-        },
-        ()
-    )
-}
+// #[test]
+// fn main_test() {
+    // let n = 9;
+    // assert_eq!(
+    //     Main {
+    //         n: n,
+    //         a: vec![
+    //             vec![0, 1, 4],
+    //             vec![1, 2, 3],
+    //             vec![2, -1, -1],
+    //             vec![3, -1, -1],
+    //             vec![4, 5, 8],
+    //             vec![5, 6, 7],
+    //             vec![6, -1, -1],
+    //             vec![7, -1, -1],
+    //             vec![8, -1, -1],
+    //         ],
+    //         pre_res: vec![None; n],
+    //         in_res: vec![None; n],
+    //         post_res: vec![None; n],
+    //     },
+    //     ()
+    // )
+// }
